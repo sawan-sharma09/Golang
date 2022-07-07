bba9886d9e92cef5688d243583e2aa37959b73e8
sawan-sharma09 <107358297+sawan-sharma09@users.noreply.github.com>
2022-07-07 20:19:28 +0530

    Update main.go

--- 02Variables/main.go
+++ 02Variables/main.go
@@ -5,17 +5,17 @@ import "fmt"
 const GlobalName = "Lets Learn"
 
 func main() {
-	// var username string = "Sawan"
-	// fmt.Println(username)
-	// fmt.Printf("variable is of type : %T\n", username)
+	 var username string = "Sawan"
+	 fmt.Println(username)
+	 fmt.Printf("variable is of type : %T\n", username)
 
-	// var isLogged bool
-	// fmt.Println(isLogged)
-	// fmt.Printf("variable is of type : %T", isLogged)
+	 var isLogged bool
+	 fmt.Println(isLogged)
+	 fmt.Printf("variable is of type : %T", isLogged)
 
-	// var smallVal uint8 = 255 // If we assign a value more than 255 to unit8, then it will throw an error (it overflows unit8)
-	// fmt.Println(smallVal)    // So in order to check the limits of any type search golang.org...>"The Go Programming Language Specification"
-	// fmt.Printf("variable is of type : %T", smallVal)
+	var smallVal uint8 = 255 // If we assign a value more than 255 to unit8, then it will throw an error (it overflows unit8)
+	fmt.Println(smallVal)    // So in order to check the limits of any type search golang.org...>"The Go Programming Language Specification"
+	fmt.Printf("variable is of type : %T", smallVal)
 
 	var smallFloat float32 = 250.97257949959596
 	fmt.Println(smallFloat)
